71419909fa480d12774e182a1c0d9d5f291cedd6
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-10-09 15:41:09 +0530

    created category schema

---
+++ models/category.js
@@ -0,0 +1,21 @@
+import { type } from "express/lib/response";
+import mongoose from "mongoose";
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Category = mongoose.model("categories", categorySchema);
+
+export default Category;
