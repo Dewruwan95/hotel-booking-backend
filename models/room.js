6f751bc46fe22cc582203cd6ebc32995b546eee8
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-10-09 21:14:54 +0530

    created room schema

---
+++ models/room.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import Category from "./category";
+
+const roomSchema = mongoose.Schema({
+  roomNo: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  category: {
+    type: {},
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  maxGuest: {
+    type: Number,
+    required: true``,
+  },
+  photos: {
+    type: Object,
+    default: {},
+    required: true,
+  },
+  isDisabled: {
+    type: Boolean,
+    default: false,
+  },
+  isBooked: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Room = mongoose.model("rooms", roomSchema);
+
+export default Room;
