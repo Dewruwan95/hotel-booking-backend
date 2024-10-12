8a3d17ba19e230240a98d84b2b223bcf65983a2e
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-10-12 15:16:24 +0530

    created category deletion function

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -40,3 +40,32 @@ export function getCategory(req, res) {
     });
   });
 }
+
+// delete category ----------------
+export function deleteCategory(req, res) {
+  const user = req.user;
+  if (user) {
+    if (user.type == "admin") {
+      const name = req.params.name;
+      Category.findOneAndDelete({ name: name })
+        .then(() => {
+          res.status(200).json({
+            message: "category deleted successfully",
+          });
+        })
+        .catch(() => {
+          res.status(400).json({
+            message: "Category deletion failed",
+          });
+        });
+    } else {
+      res.status(403).json({
+        message: "you do not have permission to create a category",
+      });
+    }
+  } else {
+    res.status(403).json({
+      message: "Please login to create a category",
+    });
+  }
+}
