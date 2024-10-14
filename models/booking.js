147e0dcbfdc30ae1b18957c3121e35245823c7ad
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-10-14 19:23:46 +0530

    created booking schema

---
+++ models/booking.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const bookingSchema = mongoose.Schema({
+  bookingId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  roomId: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "pending",
+  },
+  reason: {
+    type: String,
+    default: "",
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+    required: true,
+  },
+  notes: {
+    type: String,
+    default: "",
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default bookingSchema;
