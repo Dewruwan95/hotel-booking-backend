6dc227d80f64821e0298658c07917f4233e33dfe
Ositha Dewruwan <71217790+Dewruwan95@users.noreply.github.com>
2024-11-06 08:17:30 +0530

    fixed id issue in event router

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -12,7 +12,7 @@ const eventRouter = express.Router();
 eventRouter.post("/", createEvent);
 eventRouter.get("/", getEvents);
 eventRouter.get("/:eventId", getEventById);
-eventRouter.put("/:id", updateEventById);
+eventRouter.put("/:eventId", updateEventById);
 eventRouter.delete("/:eventId", deleteEventById);
 
 export default eventRouter;
